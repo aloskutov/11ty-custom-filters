cfab43ed5e4141b7ff699b5ad8575aaba15b20b4
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2023-11-22 22:13:56 +0700

    added date.ts

---
+++ src/date.ts
@@ -0,0 +1,164 @@
+const globalLocale: string = 'ru-RU';
+const globalTimeZone: string = 'Asia/Barnaul';
+const formatDateOptions: Intl.DateTimeFormatOptions = {
+  day: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+  timeZone: globalTimeZone,
+};
+const formatDateLongOptions: Intl.DateTimeFormatOptions = {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
+  timeZone: globalTimeZone,
+};
+const formatDateTimeOptions: Intl.DateTimeFormatOptions = {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  day: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+  timeZone: globalTimeZone,
+};
+const formatDateTimeLongOptions: Intl.DateTimeFormatOptions = {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  timeZone: globalTimeZone,
+};
+
+/**
+ * Returns Date
+ * @param {string|Date} date
+ * @returns Date
+ */
+export const toDate = (date: string | Date = new Date()): Date => {
+
+  return typeof date === 'string'
+    ? new Date(date)
+    : date;
+};
+
+/**
+ * Returns a formatted date string.
+ * @param {string|Date} date
+ * @param {string} locale
+ * @param {DateFormatOptions} options
+ * @returns {string}
+ */
+export const formatDate = (
+  date: string | Date = new Date(),
+  locale: string = globalLocale,
+  options: Intl.DateTimeFormatOptions = {}): string => {
+
+  const localOptions: Intl.DateTimeFormatOptions = {};
+
+  Object.assign(localOptions, formatDateOptions, options);
+
+  return toDate(date).toLocaleString(locale, localOptions);
+};
+
+/**
+ * Returns a date string formatted like "Вторник, 7 Ноября 2023 г".
+ * @param {string|Date} date
+ * @param {string} locale
+ * @param {DateFormatOptions} options
+ * @returns {string}
+ */
+export const formatDateLong = (
+  date: string | Date = new Date(),
+  locale: string = globalLocale,
+  options: Intl.DateTimeFormatOptions = {}): string => {
+
+  const localOptions: Intl.DateTimeFormatOptions = {};
+
+  Object.assign(localOptions, formatDateLongOptions, options);
+
+  return formatDate(date, locale, localOptions);
+};
+
+/**
+ * Returns a date string formatted like "вторник, 7 ноября 2023 г. в 14:41:06'".
+ * @param {string|Date} date
+ * @param {string} locale
+ * @param {DateFormatOptions} options
+ * @returns {string}
+ */
+export const formatDateTimeLong = (
+  date: string | Date = new Date(),
+  locale: string = globalLocale,
+  options: Intl.DateTimeFormatOptions = {}): string => {
+
+  const localOptions: Intl.DateTimeFormatOptions = {};
+
+  Object.assign(localOptions, formatDateTimeLongOptions, options);
+
+  return formatDate(date, locale, localOptions);
+};
+
+/**
+ * Returns a date string formatted like "dd.mm.yyyy, hh:mm:ss".
+ * @param {string|Date} date
+ * @returns {string}}
+ */
+export const formatDateTime = (
+  date: string | Date = new Date(),
+  locale: string = globalLocale,
+  options: Intl.DateTimeFormatOptions = {}): string => {
+
+  const localOptions: Intl.DateTimeFormatOptions = {};
+
+  Object.assign(localOptions, formatDateTimeOptions, options);
+
+  return toDate(date).toLocaleString(locale, localOptions);
+};
+
+/**
+ * Returns date string formatted to ISO "yyyy-mm-ddThh:mm:ss.000Z"
+ * @param {string|Date} date
+ * @returns {string}}
+ */
+export const formatISO = (date: string | Date = new Date()): string => {
+
+  return toDate(date).toISOString();
+};
+
+/**
+ * Returns a date string formatted like "yyyy-mm-dd".
+ * @param {string | Date} date
+ * @returns {string}
+ */
+export const formatDateNumeric = (date: string | Date = new Date()) => {
+
+  return formatDateTime(date)
+    .split(', ')[0]
+    .split('.')
+    .reverse()
+    .join('-');
+};
+
+/**
+ * Returns a time string formatted like "hh:mm:ss".
+ * @param {string | Date} date
+ * @returns {string}
+ */
+export const formatTimeNumeric = (date: string | Date = new Date()) => {
+
+  return formatDateTime(date).split(', ')[1];
+};
+
+/**
+ * Returns a time string formatted like "yyyy-mm-dd-hhmmss".
+ * @param {string | Date} date
+ * @returns {string}
+ */
+export const formatFile = (date: string | Date = new Date()) => {
+
+  return `${formatDateNumeric(date)}-${formatTimeNumeric(date).split(':').join('')}`;
+};
---
+++ tests/date.test.ts
@@ -0,0 +1,68 @@
+import {describe, expect, test, jest} from '@jest/globals';
+import * as dt from '../src/date';
+
+const date: string = '2023-11-07 14:41:06 +07:00';
+const now: string = '2023-11-22 20:08:23 +07:00';
+const dateObj: Date = new Date(date);
+const nowObj: Date = new Date(now);
+const options: Intl.DateTimeFormatOptions = {
+  timeZone: 'UTC'
+};
+
+describe('Test Dates', () => {
+  test('String to Date', () =>{
+    expect(dt.toDate(date)).toEqual(dateObj);
+  });
+
+  test('Date string to Date', () => {
+    expect(dt.toDate(new Date(date))).toEqual(dateObj);
+  });
+
+  test('Current date to Date', () => {
+    jest.useFakeTimers().setSystemTime(new Date(now));
+    expect(dt.toDate()).toEqual(nowObj);
+  });
+
+  test('Default date format', () => {
+    expect(dt.formatDate(date)).toBe('07.11.2023');
+  });
+
+  test('Long date format', () => {
+    expect(dt.formatDateLong(date)).toBe('вторник, 7 ноября 2023 г.');
+  });
+
+  test('Long date time format', () => {
+    expect(dt.formatDateTimeLong(date)).toBe('вторник, 7 ноября 2023 г. в 14:41:06');
+  });
+
+  test('Long date time format UTC', () => {
+    expect(dt.formatDateTimeLong(date, 'en', options)).toBe('Tuesday, November 7, 2023 at 07:41:06 AM');
+  });
+
+  test('Long date time format now', () => {
+    jest.useFakeTimers().setSystemTime(new Date(now));
+    expect(dt.formatDateTimeLong()).toBe('среда, 22 ноября 2023 г. в 20:08:23');
+  });
+
+  test('Local date time format', () => {
+    expect(dt.formatDateTime(date)).toBe('07.11.2023, 14:41:06');
+  });
+
+  test('Numeric date', () => {
+    expect(dt.formatDateNumeric(date)).toBe('2023-11-07');
+  });
+
+  test('Numeric time', () => {
+    expect(dt.formatTimeNumeric(date)).toBe('14:41:06');
+  });
+
+  test('ISO date format with string', () => {
+    expect(dt.formatISO(date)).toBe('2023-11-07T07:41:06.000Z');
+  });
+
+  test('file format with string', () => {
+    expect(dt.formatFile(date)).toBe('2023-11-07-144106');
+  });
+
+});
+
