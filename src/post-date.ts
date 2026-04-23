08191e3538cb8a1301f12a5df662688988618e22
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-04-23 17:10:05 +0700

    add: W3C date formats

--- src/post-date.ts
+++ src/post-date.ts
@@ -20,6 +20,17 @@ const currentOptions: Intl.DateTimeFormatOptions = {
   month: '2-digit',
 };
 
+const w3cDateOptions: Intl.DateTimeFormatOptions = {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  day: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+  timeZone: 'Asia/Barnaul',
+  timeZoneName: 'longOffset',
+};
+
 /**
  * Generated date/time string for post title
  * @param {string} dateString
@@ -49,7 +60,7 @@ export const currentDateTimeString = (): string => {
 };
 
 /**
- *
+ * Return date ISO string
  * @param {string} dateString
  * @returns {string}
  */
@@ -57,4 +68,38 @@ export const ISODate = (dateString: string): string => {
   const date: Date = new Date(dateString);
 
   return date.toISOString();
+};
+
+/**
+ * Return W3C Complete date format YYYY-MM-DD (eg 1997-07-16)
+ * @param {string} dateString
+ * @returns {string}
+ */
+export const W3CDateShort = (dateString: string): string => {
+  const dt: Date = new Date(dateString);
+  const regex = /(\d{2}).(\d{2}).(\d{4})/s;
+
+  const [, day, month, year] = dt
+    .toLocaleString(locale, w3cDateOptions)
+    .split(regex);
+
+  return `${year}-${month}-${day}`;
+};
+
+/**
+ * Return W3C complete date plus hours, minutes and seconds format
+ * YYYY-MM-DDThh:mm:ssTZD (eg 1997-07-16T19:20:30+01:00)
+ * @param {string} dateString
+ * @returns {string}
+ */
+export const W3CDateLong = (dateString: string): string => {
+  const dt: Date = new Date(dateString);
+  const regex =
+    /(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2}) GMT(\+\d{2}:\d{2})/s;
+
+  const [, day, month, year, hour, minute, second, tz] = dt
+    .toLocaleString(locale, w3cDateOptions)
+    .split(regex);
+
+  return `${year}-${month}-${day}T${hour}:${minute}:${second}${tz}`;
 };
