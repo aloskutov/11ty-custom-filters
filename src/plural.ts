352fc5a2ee12d7bcb4884c0a19536e9831e8523b
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2026-04-24 13:22:46 +0700

    update: plurals

--- src/plural.ts
+++ src/plural.ts
@@ -1,20 +1,20 @@
 const locale: string = 'ru-RU';
-const options: Intl.PluralRulesOptions = {
-  type: 'cardinal',
-};
+const options: Intl.PluralRulesOptions = {type: 'cardinal'};
 const forms: Record<string, number> = {
+  zero: 2,
   one: 0,
+  two: 1,
   few: 1,
   many: 2,
-  other: 2, // fallback for ru-RU
+  other: 2,
 };
 const pr = new Intl.PluralRules(locale, options);
 
 /**
- *
+ * Plurals
  * @param {number} quantity
  * @param {Array<string>} plurals - ['яблоко', 'яблока', 'яблок']
  * @returns {string}
  */
-export default (quantity: number, plurals: Array<string>): string =>
-  plurals[forms[pr.select(quantity)] ?? 2];
+export default (quantity: number, plurals: Array<string>[3]): string =>
+  plurals[forms[pr.select(quantity)]];
